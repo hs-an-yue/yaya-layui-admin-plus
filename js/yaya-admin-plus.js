c25ccd9d900728875c9017218248d903e672c5fb
hs <hd1611756908@163.com>
2026-03-06 15:01:07 +0800

    更新公告和按钮样式🚀

--- js/yaya-admin-plus.js
+++ js/yaya-admin-plus.js
@@ -123,7 +123,7 @@ layui.use(function (){
                 area: ['500px', '100%'],
                 shade: 0.1,
                 shadeClose: true,
-                content: 'views/bulletin-board.html'
+                content: 'views/notice-list.html'
             });
         },
         /* 主题修改 */
